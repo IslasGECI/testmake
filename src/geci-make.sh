9e02616275a14981f9ca98ec7bb6807ea246bcab
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-08-05 13:21:03 -0700

    ✨ Add first version of geci-make

---
+++ src/geci-make.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# This script is used to make all the Makefiles targets.
+#
+# Usage: ./src/geci-make.sh <REPOSITORY> <REPORT> <BRANCH>
+# Example: ./src/geci-make.sh isla-guadalupe reports/mapa_densidad_censo.pdf develop
+
+repository=$1
+report=$2
+branch=${3:-develop}
+
+function pull_repository {
+  repository=$1
+  branch=$2
+  [ ! -d "${repository}" ] && git clone git@bitbucket.org:IslasGECI/"${repository}".git
+  cd "${repository}" || return
+  git checkout "${branch}"
+  git pull
+}
+
+pull_repository "${repository}" "${branch}"
+image_tag=$(jq --raw-output --arg report "${report}" '.[] | select(.report==$report) | .image_tag' analyses.json)
+log="${report}@${repository}:${image_tag}"
+echo ""
+echo "${log}"
+echo "........................................"
+echo ""
+docke pull "islasgeci/${repository}:${image_tag}"
+docker run --env BITBUCKET_USERNAME="${BITBUCKET_USERNAME}" --env BITBUCKET_PASSWORD="${BITBUCKET_PASSWORD}" --rm --volume "${PWD}":/workdir "islasgeci/${repository}:${image_tag}" make "reports/${report}"
