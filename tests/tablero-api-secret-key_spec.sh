61cb33e431a3c16299895c3eb71e5c4955ca181d
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-28 20:50:06 +0000

    ✅ Pass one test

--- tests/tablero-api-secret-key_spec.sh
+++ tests/tablero-api-secret-key_spec.sh
@@ -6,20 +6,21 @@ Describe 'geci-testmake'
     The status should be failure
   End
 
-  It 'Verify that directory is empty'
-    export TABLERO_API_SECRET_KEY=dummy_key
-    When call src/geci-testmake hola mundo
-    The line 15 of stdout should equal '/root/.testmake/hola [ OK ]'
+  Mock mkdir
+    exit 0
   End
 
   It 'Accepts TABLERO_API_SECRET_KEY'
     export TABLERO_API_SECRET_KEY=dummy_key
     When call src/geci-testmake hola mundo
     The line 8 of stdout should equal 'La variable de entorno <TABLERO_API_SECRET_KEY> esta definida,'
+    The status should be failure
   End
 
-  Mock mkdir
-    exit 0
+   It 'Verify that directory is empty'
+    export TABLERO_API_SECRET_KEY=dummy_key
+    When call src/geci-testmake hola mundo
+    The line 15 of stdout should equal '/root/.testmake/hola [ OK ]'
   End
 
   It 'Verify that directory is not empty'
