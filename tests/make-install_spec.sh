e60c3762b944004874fcc86c068e86a9d0c9f63b
Mario Villasante <mario.villasante@islas.org.mx>
2024-05-02 12:47:27 -0700

    🔨🚚 use absolute path instead of command

--- tests/make-install_spec.sh
+++ tests/make-install_spec.sh
@@ -1,12 +1,10 @@
 Describe 'make install'
-  setup() { export PATH="$PATH:/root/.local/bin" ; }
-  BeforeAll 'setup'
   It 'copies geci-testmake'
     When call geci-testmake --help
     The output should include "Verifica que 'make' puede construir un objetivo"
   End
   It 'install dependencies'
-    When call container_runner --help
+    When call /root/.local/bin/container_runner --help
     The output should include 'container_runner'
   End
 End
