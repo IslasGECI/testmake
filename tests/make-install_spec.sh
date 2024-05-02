af8850c7a2837e21a93d16cc4f20876acc5f601f
Mario Villasante <mario.villasante@islas.org.mx>
2024-05-02 10:23:15 -0700

    💫🧪 update test

--- tests/make-install_spec.sh
+++ tests/make-install_spec.sh
@@ -1,7 +1,7 @@
 Describe 'make install'
   It 'copies geci-testmake'
     When call tests/test_make-install.sh
-    The line 6 of output should equal '/usr/local/bin/geci-testmake'
+    The line 9 of output should equal '/usr/local/bin/geci-testmake'
   End
   It 'install dependencies'
     When call container-runner --help
